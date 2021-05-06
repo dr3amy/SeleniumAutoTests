1cf255e4b74a0bc189ab930ed15c038aef34223a
Nikita Kaftanov <arterterone@gmail.com>
2021-05-07 00:42:05 +0300

    added two methods testing success message

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,7 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
+import pytest
+
 
 
 class ProductPage(BasePage):
@@ -21,3 +23,14 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.BASKET_PRICE), 'Basket price not presented'
         price2 = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
         assert price1 == price2, 'Price is not equal'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message has not disappeared, but should be"
+
+
+
