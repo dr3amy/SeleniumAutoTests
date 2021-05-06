96619d875a836d42f267b6644e4b8d4a639980e0
Nikita Kaftanov <arterterone@gmail.com>
2021-05-06 23:26:53 +0300

    fixed locators

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,11 @@ class LoginPageLocators:
     REG_USERNAME = (By.CSS_SELECTOR, "#id_registration-email")
     REG_PASS = (By.CSS_SELECTOR, "#id_registration-password1")
     REG_PASS_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    BASKET_PRODUCT_NAME = (By.CSS_SELECTOR, '#messages .alertinner strong')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    BASKET_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
---
+++ pages/product_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button.click()
+
+    def should_be_right_product(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), 'Product name not presented'
+        elm1 = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert self.is_element_present(*ProductPageLocators.BASKET_PRODUCT_NAME), 'Basket product name not presented'
+        elm2 = self.browser.find_element(*ProductPageLocators.BASKET_PRODUCT_NAME).text
+        assert elm1 == elm2, 'Wrong product added to basket'
+
+    def should_be_right_price(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), 'Product price not presented'
+        price1 = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        assert self.is_element_present(*ProductPageLocators.BASKET_PRICE), 'Basket price not presented'
+        price2 = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        assert price1 == price2, 'Price is not equal'
