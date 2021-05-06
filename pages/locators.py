30d07a7e1a98eb067c85eb0532287a07ee3796e1
Nikita Kaftanov <arterterone@gmail.com>
2021-05-07 00:41:20 +0300

    added product page locator for success message

--- pages/locators.py
+++ pages/locators.py
@@ -19,3 +19,4 @@ class ProductPageLocators:
     BASKET_PRODUCT_NAME = (By.CSS_SELECTOR, '#messages .alertinner strong')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
     BASKET_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alertinner strong')
